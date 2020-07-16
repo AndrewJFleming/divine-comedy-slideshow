86eabda7394ce51dc64dbe69b6b3fed325768015
AndrewJFleming <aflemi1@gmail.com>
2020-07-16 12:40:14 -0700

    Updates

---
+++ precache-manifest.7b346da8fafb4088730f4ff22eedba8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e76ddafd4e8cfaea1cc922798b1f4af",
+    "url": "/divine-comedy-slideshow/index.html"
+  },
+  {
+    "revision": "c32e09c2ae7f749f06ea",
+    "url": "/divine-comedy-slideshow/static/css/main.451254bc.chunk.css"
+  },
+  {
+    "revision": "6b1d5ff39ffb88b5dd75",
+    "url": "/divine-comedy-slideshow/static/js/2.44fc6a60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/divine-comedy-slideshow/static/js/2.44fc6a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32e09c2ae7f749f06ea",
+    "url": "/divine-comedy-slideshow/static/js/main.5b4e7139.chunk.js"
+  },
+  {
+    "revision": "a2767b708b31baddee07",
+    "url": "/divine-comedy-slideshow/static/js/runtime-main.e514c0cc.js"
+  }
+]);
